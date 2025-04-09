7dde371978d5e203e649da807ce828c7e6826912
kurissu <kris.percussion@gmail.com>
2025-04-09 17:46:24 +0700

    test comit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -44,7 +44,7 @@ const Experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Flutter</h4>
-                <small className="text-light">Basic</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__detail">
